09410ecf5da7f01cf27aa262d965ca886b342d39
Ju Liu <ju@noredink.com>
2017-12-13 20:30:37 +0000

    Refactor player game

--- lib/pokerwars/game.ex
+++ lib/pokerwars/game.ex
@@ -1,7 +1,9 @@
 defmodule Pokerwars.Game do
-  alias Pokerwars.{Deck}
+  alias Pokerwars.{Deck, Player}
 
   defstruct players: [], status: :waiting_for_players, current_deck: nil, original_deck: nil
+
+  @max_players 2
 
   def create(deck \\ Deck.in_order) do
     {
@@ -15,28 +17,32 @@ defmodule Pokerwars.Game do
   end
 
   defp phase(:waiting_for_players, game, action) do
-    waiting_for_players(action, game)
+    with {:ok, game} <- waiting_for_players(action, game),
+         {:ok, game} <- next_status(game)
+    do
+      {:ok, game}
+    else
+      err -> err
+    end
   end
-
   defp phase(:ready_to_start, game, action) do
     ready_to_start(action, game)
   end
 
-  defp waiting_for_players({:join, player}, game) do
-    players = game.players ++ [player]
-    enough_players = length(players) >= 2
-    status = if enough_players, do: :ready_to_start, else: :waiting_for_players
-    game = %{ game | players: players, status: status }
-    {:ok, game}
+  defp next_status(%__MODULE__{status: :waiting_for_players, players: players} = game)
+  when length(players) == @max_players do
+    {:ok, %{game | status: :ready_to_start}}
+  end
+  defp next_status(game), do: {:ok, game}
+
+  defp waiting_for_players({:join, player}, %{players: players} = game)
+  when length(players) < @max_players do
+    {:ok, %{game | players: game.players ++ [player]}}
   end
   defp waiting_for_players(_, game), do: {:invalid_action, game}
 
   defp ready_to_start({:start_game}, game) do
-    game = deal_hands(game)
-
-    game = %{game | status: :started}
-
-    {:ok, game}
+    {:ok, %{deal_hands(game) | status: :started}}
   end
 
   defp deal_hands(game) do
@@ -46,14 +52,11 @@ defmodule Pokerwars.Game do
   end
 
   defp clear_table(game) do
-    players = Enum.map(game.players, fn player -> put_in(player.hand, []) end)
+    players = Enum.map(game.players, &Player.clear_hand/1)
     %{game | current_deck: game.original_deck, players: players}
   end
 
-  defp deal_two_cards_to_each_player(game) do
-    players = game.players
-    deck = game.current_deck
-
+  defp deal_two_cards_to_each_player(%{players: players, current_deck: deck} = game) do
     deal_hand = fn (player, {deck, players}) ->
       {deck, player} = deal_card_to_player(deck, player)
       {deck, players ++ [player]}
@@ -67,8 +70,14 @@ defmodule Pokerwars.Game do
 
   defp deal_card_to_player(deck, player) do
     {card, deck} = Deck.deal(deck)
-    hand = player.hand ++ [card]
-    player = %{ player | hand: hand }
+    player = Player.add_card_to_hand(player, card)
     {deck, player}
+  end
+
+  defp deal_cards_to_players(deck, players) do
+    Enum.reduce(players, {deck, []}, fn (player, {current_deck, current_players}) ->
+      {new_deck, new_player} = deal_card_to_player(current_deck, player)
+      {new_deck, current_players ++ [new_player]}
+    end)
   end
 end
