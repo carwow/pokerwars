bbc2f7bb876a09589d5c801744b8d8f0516e1a83
David Santoro <soulnafein@gmail.com>
2017-12-19 23:06:14 +0000

    Fixed paying blinds test

--- test/pokerwars/paying_blinds_test.exs
+++ test/pokerwars/paying_blinds_test.exs
@@ -1,21 +1,15 @@
 defmodule Pokerwars.PayingBlindsTest do
   use ExUnit.Case, async: true
   import Pokerwars.TestHelpers
-  alias Pokerwars.{Player,Game, Deck}
+  alias Pokerwars.{Game}
 
   test "Players pay blinds" do
-    {:ok, game} = Game.create
-
-    david = Player.create("David", 100)
-    {:ok, game} = Game.apply_action(game, {:join, david})
-
-    jade = Player.create("Jade", 100)
-    {:ok, game} = Game.apply_action(game, {:join, jade})
+    players = [%{david() | stack: 100}, %{jade() | stack: 100}]
+    game = %{ Game.create | players: players, status: :ready_to_start }
 
     {:ok, game} = Game.apply_action(game, {:start_game})
 
     stacks = Enum.map(game.players, &(&1.stack))
-
     assert stacks == [90, 80]
   end
 end
